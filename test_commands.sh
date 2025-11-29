d8b26de10ccacef825995caf9f44cd92ebccc728
AlishaRP2005 <rpalisha86@gmail.com>
2025-11-29 12:03:31 +0000

    add, complete , read,creation

--- test_commands.sh
+++ test_commands.sh
@@ -3,13 +3,13 @@
 ##################### API Observation Via CURL
 
 # A. Get All Students
-curl -X GET "https://automatic-xylophone-jjvrjvxw7p65cxr-8000.app.github.dev/"
+curl -X GET "https://automatic-xylophone-jjvrjvxw7p65cxr-8000.app.github.dev/api/students"
 
 # B. Get One Student
 curl -X GET "http://localhost:8000/api/students/1"
 
 # C. Create Student
-curl -X POST "https://automatic-xylophone-jjvrjvxw7p65cxr-8000.app.github.dev/" \
+curl -X POST "https://automatic-xylophone-jjvrjvxw7p65cxr-8000.app.github.dev/api/students" \
   -H "Content-Type: application/json" \
   -d '{
     "name": "Alice Johnson",
