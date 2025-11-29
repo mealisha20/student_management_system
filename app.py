1470ebeac5dffaab9cd15d2430f2c442ae60be0f
AlishaRP2005 <rpalisha86@gmail.com>
2025-11-29 05:07:59 +0000

    Feat: add database connectivity

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from router import StudentRouter
 from database.connection import init_database
 
 def run_server(port=8000):
- 
-    server = HTTPServer(("",port),StudentRouter)
+    init_database()
+    server = HTTPServer(("",port), StudentRouter)
     print(f"🚀server running at http://localhost:{port}")
     server.serve_forever()
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
         run_server()
